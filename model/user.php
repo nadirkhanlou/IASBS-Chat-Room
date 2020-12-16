730ca836ce8fdf0302ff9c26b444e9509678dc42
arv78 <arv78@ymail.com>
2020-12-16 11:21:33 +0330

    added class user to model

--- model/user.php
+++ model/user.php
@@ -21,4 +21,35 @@ abstract class person {
     }
 }
 
+class user extends person
+{
+    private $username;
+    private $password;
+    private $cellphone;
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function setPassword($password) {
+        $this->password = md5($password);
+    }
+
+    function getCellphone() {
+        return $this->cellphone;
+    }
+
+    function setCellphone($cellphone) {
+        $this->cellphone = $cellphone;
+    }
+}
+
 ?>
