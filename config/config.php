e42e5970360f0681db235d760ff287e2892cfaa2
arv78 <arv78@ymail.com>
2020-12-16 18:42:13 +0330

    adding simple connection to DB & their configuration requirements

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+$server = 'localhost';
+$db_username = 'root';
+$db_password = '123456';
+$db_name = 'users';
+?>
---
+++ data base/DB.php
@@ -0,0 +1,19 @@
+<?php
+abstract class database {
+    private static function ConnectToDB()
+    {
+        global $server;
+        global $db_username;
+        global $db_password;
+        global $db_name;
+
+        $connection = mysqli_connect($server, $db_username, $db_password, $db_name);
+        if ($connection->connect_error)
+        {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        return $connection;
+    }
+}
+
+?>
