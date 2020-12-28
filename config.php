dd395b04fe5c93cc5f89ad61d763bec95d2b7105
nadirkhanlou <39118903+nadirkhanlou@users.noreply.github.com>
2020-12-28 11:02:06 +0330

    Added config.php and index.php
    * Added index.php just to see how the main page after login would look like.
    * config.php contains necessary passes and other info which are not used yet (like database info)

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$server = 'localhost';
+$db_username = 'root';
+$db_password = '123456';
+$db_name = 'whosapp';
+
+$ViewPath = 'view/';
+$SharedFolderPath = 'view/shared/';
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require "config.php";
+
+$WelcomeMessage = "Welcome!";
+
+include $SharedFolderPath."header.html";
+include $SharedFolderPath."content.html";
+include $SharedFolderPath."footer.html";
+?>
