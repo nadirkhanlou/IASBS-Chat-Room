1e00b88b083d0e7afedefa081769df54f1b550d6
Mohammadmahdi Alijani <35420218+mma1377@users.noreply.github.com>
2020-12-31 13:03:02 +0330

    user.php has been created
    user.php contains the user class

---
+++ model/user.php
@@ -0,0 +1,81 @@
+<?php
+
+class user
+{
+	private $fullName;
+	private $handle;
+	private $phoneNumber;
+	private $password;
+	
+	/*
+	The constructor consider the password is hashed by default, otherwise it can be noticed by
+	isPasswordHashed parameter so the constructor will hash it before storing it  
+	*/
+	
+	function __construct($fullName, $handle, $phoneNumber, $password, $isPasswordHashed = true)
+	{
+		$this->fullName = $fullName;
+		$this->handle = $handle;
+		$this->phoneNumber = $phoneNumber;
+		$this->password = isPasswordHashed ? $password : HashPassword();
+	}
+	
+	
+	//Getters
+	
+	public function GetFullName ()
+	{
+		return $this->fullName;
+	}
+	
+	public function GetHandle ()
+	{
+		return $this->handle;
+	}
+	
+	public function GetPhoneNumber ()
+	{
+		return $this->phoneNumber;
+	}
+	
+	public function GetPassword ()
+	{
+		return $this->password;
+	}
+	
+	//Static methods
+	
+	static function HashPassword($password)
+    {
+        return md5($password);
+    }
+	
+	static function IsUsernameExist()
+	{
+		
+	}
+	
+	//Other methods
+	
+	function StoreInDatebase()
+	{
+		
+	}
+	
+	function CheckPassword()
+	{
+		
+	}
+	
+	function GetMessages()
+	{
+		
+	}
+	
+	function SendMessage($receiverHandle, $messageText)
+	{
+		
+	}
+}
+
+>
