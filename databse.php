9b1829ab0243bc3af4737fa4b7bc40ffc02b53d5
Mohammadmahdi Alijani <35420218+mma1377@users.noreply.github.com>
2020-12-31 13:33:57 +0330

    database.php has been created

---
+++ databse.php
@@ -0,0 +1,30 @@
+<?php
+
+static class database
+{
+    private static function ConnectToDB()
+    {
+        global $server;
+        global $db_username;
+        global $db_password;
+        global $db_name;
+
+        $connection = mysqli_connect($server, $db_username, $db_password, $db_name);
+        if ($connection->connect_error)
+        {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        return $connection;
+    }
+
+    public static function ExecuteQuery($query)
+    {
+        $connection = database::ConnectToDB();
+        $result = mysqli_query($connection, $query);
+        if(!$result)
+            die(mysqli_error($connection));
+        mysqli_close($connection);
+        return $result;
+    }
+}
+?>
