f59555d806c376e6f19fc4c24b9659be738ec836
nadirkhanlou <39118903+nadirkhanlou@users.noreply.github.com>
2021-01-02 14:42:18 +0330

    Added a logout button to the main page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function() {
     $('.user-settings-wrapper').hide();
-    $('.user-overview button').on('click', function() {
+    $('#user-overview-settings-button').on('click', function() {
         $('.user-contacts-wrapper, .user-settings-wrapper').slideToggle();
+    });
+
+    $('#user-overview-logout-button').on('click', function() {
+        // Code for the logout process
     });
 });
