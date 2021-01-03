279b29199a24a3c03e31dc91fff9ed2e8d7321cb
Mohammadmahdi Alijani <35420218+mma1377@users.noreply.github.com>
2021-01-03 10:47:33 +0330

    contacts.js has been removed and GetContacts function has been added to jQuery ready event

--- js/main.js
+++ js/main.js
@@ -28,4 +28,43 @@ $(function() {
             }
         });
     });
+
+    $('#user-contacts-wrapper-div').ready(function() {
+        $.ajax({
+            url: 'services/contacts.php',
+            type: 'POST',
+            async: !1,
+            data: {'contacts': 1},
+            success: function (resultString) {
+                result = JSON.parse(resultString);
+                if(result["success"])
+                {
+                    let contacts = result["users"]["contacts"];
+                    let blocked = result["users"]["blocked"];
+    
+                    let contactsList = document.getElementsByClassName('user-contacts-list')[0];
+                    let contactsListInner = "";
+                    for(let i = 0; i < contacts.length; ++i)
+                    {
+                        contactsListInner += `<li><span>${contacts[i]['Handle']}</span><span>${contacts[i]['FullName']}</span></li>\n`;
+                    }
+                    for(let i = 0; i < blocked.length; ++i)
+                    {
+                        contactsListInner += `<li class="contact-blocked"><span>${blocked[i]['Handle']}</span><span>${blocked[i]['FullName']}</span></li>\n`;
+                    }
+                    contactsList.innerHTML = contactsListInner;
+    
+                    console.log(contacts);
+                }
+                else
+                {
+                    console.log(result["errorMessage"]);
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown); 
+            }
+        });
+    });
 });
